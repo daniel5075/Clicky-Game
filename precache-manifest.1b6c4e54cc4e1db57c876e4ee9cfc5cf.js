a8e051b59c8032532103caadc21f02eb0fbe3988
Daniel Groveman <daniel5075@gmail.com>
2020-02-05 20:05:58 -0600

    Updates

---
+++ precache-manifest.1b6c4e54cc4e1db57c876e4ee9cfc5cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c62cec119b16ef5aeca60d5ceffd41b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "b2cd1ae578ca179b36f2",
+    "url": "/Clicky-Game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc5e2e816fbad40007cc",
+    "url": "/Clicky-Game/static/js/2.1f831936.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.1f831936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cd1ae578ca179b36f2",
+    "url": "/Clicky-Game/static/js/main.d74f46a8.chunk.js"
+  },
+  {
+    "revision": "2888ce4e99df7d1572f8",
+    "url": "/Clicky-Game/static/js/runtime-main.8eea2c5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
